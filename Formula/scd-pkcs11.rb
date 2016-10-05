5851f1e1fd05ff90b250bb6b73e263431fd168b1
bef <bef@users.noreply.github.com>
2016-10-05 11:33:46 +0200

    new version 0.0.1

--- Formula/scd-pkcs11.rb
+++ Formula/scd-pkcs11.rb
@@ -1,9 +1,9 @@
 class ScdPkcs11 < Formula
   desc "PKCS#11 provider with smart card support via GnuPG"
   homepage "https://github.com/sektioneins/scd-pkcs11"
-  url "https://github.com/sektioneins/scd-pkcs11/archive/500c01cc0a84ecc70ed53fc54783babfdf3428ab.tar.gz"
-  version "0.0"
-  sha256 "7b888a881eb3ff8711254a5b67fd218fda19fc077da44a9933bc8f8c9eb149e7"
+  url "https://github.com/sektioneins/scd-pkcs11/archive/f2a9da74a338e43c3bf6885427261ecb920cc3d5.zip"
+  version "0.0.1"
+  sha256 "a1bd1f034dcbaf571c8c973e8e9332505a2c3e09f224742a021367a7a08cbb7f"
 
   depends_on "libressl"
   depends_on "libassuan"
